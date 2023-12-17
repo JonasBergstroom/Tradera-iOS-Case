572746a3eeec49d4d0a1591435d781f35a6e2454
Jonas Bergström <jontrian97@gmail.com>
2023-12-17 10:10:02 +0100

    Initial Commit

---
+++ Tradera-iOS-Case-Jonas-Bergström/Tradera_iOS_Case_Jonas_Bergstro_mApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tradera_iOS_Case_Jonas_Bergstro_mApp.swift
+//  Tradera-iOS-Case-Jonas-Bergström
+//
+//  Created by Jonas Bergström on 2023-12-17.
+//
+
+import SwiftUI
+
+@main
+struct Tradera_iOS_Case_Jonas_Bergstro_mApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
