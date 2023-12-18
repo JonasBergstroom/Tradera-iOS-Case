0416dcf85b12ca3fac76b0ce72257a8b63663e48
Jonas Bergström <jontrian97@gmail.com>
2023-12-18 12:39:29 +0100

    Add ProductList to contain the list of products

---
+++ Tradera-iOS-Case-Jonas-Bergström/Modeö/ProductList.swift
@@ -0,0 +1,12 @@
+//
+//  ProductList.swift
+//  Tradera-iOS-Case-Jonas-Bergström
+//
+//  Created by Jonas Bergström on 2023-12-18.
+//
+
+import Foundation
+
+struct ProductList: Codable {
+    let products: [Product]
+}
